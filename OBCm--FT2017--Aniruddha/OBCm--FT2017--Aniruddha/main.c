04025dba02c592f741affaa0fc5c8b301b3b1018
Aniruddha Ranade <science.ranade@gmail.com>
2017-07-28 16:39:46 +0530

    First Commit

---
+++ OBCm--FT2017--Aniruddha/OBCm--FT2017--Aniruddha/main.c
@@ -0,0 +1,34 @@
+/*
+ * OBCm--FT2017--Aniruddha.c
+ *
+ * Created: 28-07-2017 16:35:31
+ * Author : Aniruddha Ranade
+ */ 
+
+#define F_CPU 8000000
+#include <avr/io.h>
+#include "common.h"
+#include "timer.h"
+#include "uart.h"
+#include "spi.h"
+#include "i2c.h"
+#include "led_tests.h"
+
+
+int main(void)
+{
+    // Initializations
+    
+    // Variables
+    
+    sei();
+	
+	while (1) 
+    {
+		//Loop
+		
+		
+		
+    }
+}
+
