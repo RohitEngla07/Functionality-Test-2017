1e6f43e62dd6febd06122e4c9a32ffb07d44c084
Aniruddha Ranade <science.ranade@gmail.com>
2017-08-01 23:01:54 +0530

    Power Code - basic files included

--- OBCm-Power--FT2017--Aniruddha/Power--OBCm-Power/Power--OBCm-Power/main.c
+++ OBCm-Power--FT2017--Aniruddha/Power--OBCm-Power/Power--OBCm-Power/main.c
@@ -5,13 +5,27 @@
  * Author : Aniruddha Ranade
  */ 
 
+#define F_CPU 8000000
 #include <avr/io.h>
+#include "common.h"
+#include "uart.h"
+#include "i2c.h"
 
 
 int main(void)
 {
+    // Initializations
+    
+    // Variables
+    
+    sei();
+		
     while (1) 
     {
+		
+		//loop
+		
+		
     }
 }
 
