f8f2f420d219427dadeaa7ec9027a049bd0b95fc
Aniruddha Ranade <science.ranade@gmail.com>
2017-08-01 21:47:24 +0530

    OBCm Power Code

---
+++ OBCm-Power--FT2017--Aniruddha/OBCm--OBCm-Power/OBCm--OBCm-Power/main.c
@@ -0,0 +1,17 @@
+/*
+ * OBCm--OBCm-Power.c
+ *
+ * Created: 01-08-2017 21:45:03
+ * Author : Aniruddha Ranade
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    while (1) 
+    {
+    }
+}
+
---
+++ OBCm-Power--FT2017--Aniruddha/Power--OBCm-Power/Power--OBCm-Power/main.c
@@ -0,0 +1,17 @@
+/*
+ * Power--OBCm-Power.c
+ *
+ * Created: 01-08-2017 21:46:08
+ * Author : Aniruddha Ranade
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    while (1) 
+    {
+    }
+}
+
