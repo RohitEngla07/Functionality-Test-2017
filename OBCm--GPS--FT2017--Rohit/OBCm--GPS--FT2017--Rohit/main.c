b758e950d474f1c0612c1285d2e612db4c46a7ab
rohitengla07 <rohitengla07@gmail.com>
2017-08-07 23:23:04 +0530

    Rohit GPS code first commit

---
+++ OBCm--GPS--FT2017--Rohit/OBCm--GPS--FT2017--Rohit/main.c
@@ -0,0 +1,18 @@
+/*
+ * OBCm--GPS--FT2017--Rohit.c
+ *
+ * Created: 07-08-2017 21:59:36
+ * Author : Rohit Engl
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
